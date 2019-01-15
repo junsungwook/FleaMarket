72696ceb0449688bd6dc4a9ddf8af7e3f0f3ae51
jeonjuyeong <junjy93@naver.com>
2019-01-15 11:48:49 +0900

    ssss

---
+++ src/action/DeleteAction.java
@@ -0,0 +1,62 @@
+package action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.BoardDAO;
+
+/**
+ * Servlet implementation class DeleteAction
+ */
+@WebServlet("/board/delete")
+public class DeleteAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		int BOARD_NUM = Integer.parseInt(request.getParameter("BOARD_NUM"));
+		request.setAttribute("BOARD_NUM", BOARD_NUM);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("deleteForm.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		int BOARD_NUM = Integer.parseInt(request.getParameter("BOARD_NUM"));
+		String BOARD_PASS = request.getParameter("BOARD_PASS");
+		BoardDAO dao  = BoardDAO.getInstance();
+		boolean resp = dao.delBoard(BOARD_NUM,BOARD_PASS);
+		if(resp==false){
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+            out.println("<script>alert('비밀번호가 다릅니다'); history.go(-1);</script>");
+            out.flush(); 
+		}
+		else{
+			response.sendRedirect("boardList.bo");
+		}
+	}
+
+}
---
+++ src/action/ListAction.java
@@ -0,0 +1,78 @@
+package action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.BoardDAO;
+import vo.BoardVO;
+
+/**
+ * Servlet implementation class ListAction
+ */
+@WebServlet("/board/boardList.bo")
+public class ListAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		BoardDAO dao = BoardDAO.getInstance();
+	
+		String pageNum = request.getParameter("pageNum")==null?"1":request.getParameter("pageNum");
+		int currentPage = Integer.parseInt(pageNum);
+		int pageSize = 5;
+		int startRow = (currentPage-1)*pageSize+1; //2page -> 6번댓글부터
+		int endRow = currentPage*pageSize;
+		int count = dao.boardCount();
+		
+		ArrayList<BoardVO> arr = dao.boardList(startRow,endRow);
+		
+		
+		//총페이지수
+		int totpage = count/pageSize+(count%pageSize==0?0:1);
+		int blockpage =3; //[이전] 456 [다음]
+		int startpage=((currentPage-1)/blockpage)*blockpage+1;
+		int endpage=startpage+blockpage-1;
+		
+		if(endpage > totpage) endpage=totpage;
+		
+		request.setAttribute("totpage", totpage);
+		request.setAttribute("startpage", startpage);
+		request.setAttribute("endpage", endpage);
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("blockpage", blockpage);
+		
+		int number=count-(currentPage-1)*pageSize;
+		request.setAttribute("number",number);
+		request.setAttribute("lists", arr);
+		request.setAttribute("count", count);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("boardList.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
