42338a48dabd41f7197584a257ecf56c4cc0a055
junsungwook <45280964+junsungwook@users.noreply.github.com>
2019-01-16 14:04:57 +0900

    goodsInsert만드는중

---
+++ src/vo/goodsDAO.java
@@ -0,0 +1,21 @@
+package vo;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class goodsDAO {
+	private static goodsDAO instance = new goodsDAO();
+	public static goodsDAO getInstance() {
+		return instance;
+	}
+	private Connection getConnection() throws Exception{
+		Context initCtx = new InitialContext();
+		Context envCtx = (Context)initCtx.lookup("java:comp/env");
+		DataSource ds = (DataSource)envCtx.lookup("jdbc/board");
+		return ds.getConnection();
+	}
+	
+}
