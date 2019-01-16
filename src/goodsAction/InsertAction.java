3d1dcd41102166330cf4f6906e7632b8d31863f4
KimWonBIni <40154895+KimWonBIni@users.noreply.github.com>
2019-01-16 14:55:53 +0900

    main 절대주소로 변경
    main 절대주소

--- src/goodsAction/InsertAction.java
+++ src/goodsAction/InsertAction.java
@@ -69,14 +69,14 @@ public class InsertAction extends HttpServlet {
 			System.out.println(summernote);
 			goods.setMainpic(mainpic);
 			goodsDAO dao = goodsDAO.getInstance();
-			dao.goodsInsert(goods);
+		/*	dao.goodsInsert(goods);*/
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
 		response.sendRedirect("CSSRecipe.do");
 	}
-	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
