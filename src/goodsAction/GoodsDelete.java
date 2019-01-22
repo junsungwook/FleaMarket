2a44328442d55256fbfac0c57c405d7cb20a3faa
junsungwook <45280964+junsungwook@users.noreply.github.com>
2019-01-22 14:18:20 +0900

    수정중

--- src/goodsAction/GoodsDelete.java
+++ src/goodsAction/GoodsDelete.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import vo.goodsDAO;
+import vo.goodsDTO;
+
 /**
  * Servlet implementation class GoodsDelete
  */
@@ -26,7 +29,11 @@ public class GoodsDelete extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.setCharacterEncoding("utf-8");
+		int num = Integer.parseInt(request.getParameter("num"));
+		goodsDAO dao = goodsDAO.getInstance();
+		dao.goodsDelete(num);
+		response.sendRedirect("../fm/shop.jsp");
 	}
 
 	/**
